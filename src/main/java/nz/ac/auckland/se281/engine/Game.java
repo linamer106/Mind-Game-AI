3b01579efab1f0ca237f2a5e7193215e2b6e0a1d
Lina Amer <lina088@aucklanduni.ac.nz>
2025-05-20 12:31:51 +1200

    for some reason failing test 3 and 4 now

--- src/main/java/nz/ac/auckland/se281/engine/Game.java
+++ src/main/java/nz/ac/auckland/se281/engine/Game.java
@@ -2,6 +2,8 @@ package nz.ac.auckland.se281.engine;
 
 import nz.ac.auckland.se281.Main.Difficulty;
 import nz.ac.auckland.se281.cli.MessageCli;
+import nz.ac.auckland.se281.cli.Utils;
+import nz.ac.auckland.se281.model.Colour;
 
 public class Game {
   public static String AI_NAME = "HAL-9000";
@@ -9,6 +11,8 @@ public class Game {
   private int numRounds;
   private String[] options;
   private int roundNumber = 1;
+  private String input;
+  private Colour inputColour;
 
   public Game() {}
 
@@ -20,15 +24,39 @@ public class Game {
     MessageCli.WELCOME_PLAYER.printMessage(namePlayer);
   }
 
+  // public void play() {
+  //   if (roundNumber <= numRounds) {
+  //     MessageCli.START_ROUND.printMessage(roundNumber, numRounds);
+  //     roundNumber++;
+  //     MessageCli.ASK_HUMAN_INPUT.printMessage();
+  //     String input = Utils.scanner.nextLine();
+  //     inputColour = Colour.fromInput(input);
+  //     if (inputColour == null) {
+  //       MessageCli.INVALID_HUMAN_INPUT.printMessage();
+  //           roundNumber--;
+  //       return;
+  //     }
+
+  //   } else {
+  //     // show who won
+  //   }
+  // }
+
   public void play() {
     if (roundNumber <= numRounds) {
       MessageCli.START_ROUND.printMessage(roundNumber, numRounds);
-      roundNumber++;
       MessageCli.ASK_HUMAN_INPUT.printMessage();
+      String input = Utils.scanner.nextLine();
+      inputColour = Colour.fromInput(input);
+      if (inputColour == null) {
+        MessageCli.INVALID_HUMAN_INPUT.printMessage();
+        return;
+      }
+      roundNumber++;
+
     } else {
       // show who won
     }
-    // String line = Utils.scanner.nextLine();
   }
 
   public void showStats() {}
